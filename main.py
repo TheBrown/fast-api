5ccc18b98b0659a897c3c6b197bb0a9075a8f849
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-05 23:44:49 +0700

    Path parameters

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI
 
-app = FastAPI()
+class ModelName(str, Enum):
+    bobby = "bobby"
+    alexis = "alexis"
+    novak = "novak"
 
+
+app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World", "message": "Play with fast api"}
 
-
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id":  user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.bobby:
+        return {"model_name": model_name, "message": "Hi Bobby model!"}
+    if model_name is ModelName.novak:
+        return {"model_name": model_name, "message": "Hi Novak model!"}
+    return {"model_name": model_name, "message": "there are some problems with this model"}
