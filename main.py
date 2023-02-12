51799787bd3bd40080f90c1e1e1aef427f5dcf69
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-12 09:53:00 +0700

    Extra Data Types

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union, List
 from enum import Enum
 from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel, Field, HttpUrl
+from uuid import UUID
+from datetime import datetime, time, timedelta
 
 fake_items_db = [{"item_name": "Love"}, {
     "item_name": "You"}, {"item_name": "My Heart"}]
@@ -94,42 +96,24 @@ async def read_items(
 
 
 @app.put("/items/{item_id}")
-async def update_item(
-        *,
-        item_id: int,
-        item: Item = Body(examples={
-            "normal": {
-                "summary": "A normal example",
-                "description": "A **normal** item works correctly.",
-                "value": {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                },
-            },
-            "converted": {
-                "summary": "An example with converted data",
-                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                "value": {
-                    "name": "Bar",
-                    "price": "35.4",
-                },
-            },
-            "invalid": {
-                "summary": "Invalid data is rejected with an error",
-                "value": {
-                    "name": "Baz",
-                    "price": "thirty five point four",
-                },
-            },
-        }),
-        user: User
+async def read_items(
+        item_id: UUID,
+        start_datetime: Union[datetime, None] = Body(default=None),
+        end_datetime: Union[datetime, None] = Body(default=None),
+        repeat_at: Union[time, None] = Body(default=None),
+        process_after: Union[timedelta, None] = Body(default=None),
 ):
-    results = {"item_id": item_id, "user": user}
-    if item:
-        results.update({"item": item})
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_datetime
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration
+    }
 
 
 @app.post("/items")
