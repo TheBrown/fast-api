7d3bb92f0187e6c41e128f354bd3c99b80924f5c
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-20 22:00:59 +0700

    Tags Metadata

--- app/main.py
+++ app/main.py
@@ -55,21 +55,22 @@ You will be able to:
 * **Read users** (_not implemented_).
  """
 
-app = FastAPI(
-    title="ChimichangeApp",
-    description=description,
-    version="0.0.1",
-    terms_of_service="http://example.com/terms",
-    contact={
-        "name": "Deadpoolio the Amazing",
-        "url": "http://x-force.example.com/contact/",
-        "email": "dp@x-force.example.com"
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
     },
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/"
+        }
     }
-)
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
 
 origins = [
     "http://localhost.tianglo.com",
