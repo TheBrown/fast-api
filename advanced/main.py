bc0916f203fc21b903f40eaa0133a2b232156a91
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-21 22:23:26 +0700

    Custom OpenAPI content type

--- advanced/main.py
+++ advanced/main.py
@@ -1,41 +1,36 @@
-from fastapi import FastAPI, Request
+from typing import List
+
+import yaml
+from fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel, ValidationError
 
 app = FastAPI()
 
 
-def magic_data_reader(raw_body: bytes):
-    return {
-        "size": len(raw_body),
-        "content": {
-            "name": "Maaaagic",
-            "price": 42,
-            "description": "Just kiddin', magic here ✨",
-        }
-    }
+class Item(BaseModel):
+    name: str
+    tags: List[str]
 
 
 @app.post(
     "/items",
     openapi_extra={
         "requestBody": {
-            "content": {
-                "application/json": {
-                    "schema": {
-                        "required": ["name", "price"],
-                        "type": "object",
-                        "properties": {
-                            "name": {"type": "string"},
-                            "price": {"type": "number"},
-                            "description": {"type": "string"},
-                        }
-                    }
-                }
-            },
+            "content": {"application/x-yaml": {"schema": Item.schema()}},
             "required": True
-        },
+        }
+
     }
 )
 async def create_item(request: Request):
     raw_body = await request.body()
-    data = magic_data_reader(raw_body)
-    return data
+    try:
+        data = yaml.safe_load(raw_body)
+    except yaml.YAMLError:
+        raise HTTPException(status_code=422, detail="Invalid YAML")
+    try:
+        item = Item.parse_obj(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=e.errors())
+    return item
+
