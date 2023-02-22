23a8439a1e34e846d1a69081e59682692db9981f
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-22 21:38:22 +0700

    Use ORJSONResponse

--- advanced/main.py
+++ advanced/main.py
@@ -1,18 +1,9 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 app = FastAPI()
 
 
-@app.get("/legacy")
-def get_legacy_data():
-    data = """<?xml version="1.0"?>
-    <shampoo>
-    <Header>
-        Apply shampoo here.
-    </Header>
-    <Body>
-        You'll have to use soap here.
-    </Body>
-    </shampoo>
-    """
-    return Response(content=data, media_type="application/xml")
+@app.get("/items", response_class=ORJSONResponse)
+async def read_items():
+    return ORJSONResponse([{"item_id": "Foo"}])
