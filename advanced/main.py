20c7e60e2a467c09fec302035528d32c4486cd23
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-22 21:25:48 +0700

    Using the jsonable_encoder in a Response

--- advanced/main.py
+++ advanced/main.py
@@ -1,25 +1,22 @@
+from datetime import datetime
 from typing import Union
 
-from fastapi import FastAPI, status, Body
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Union[str, None] = None
+
 
 app = FastAPI()
 
-items = {"foo": {"name": "Fighters", "size": 6}, "bar": {"name": "Tenders", "size": 3}}
 
-
-@app.put("/items/{item_id}")
-async def upsert_item(
-        item_id: str,
-        name: Union[str, None] = Body(default=None),
-        size: Union[int, None] = Body(default=None)
-):
-    if item_id in items:
-        item = items[item_id]
-        item["name"] = name
-        item["size"] = size
-        return item
-    else:
-        item = {"name": name, "size": size}
-        items[item_id] = item
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
