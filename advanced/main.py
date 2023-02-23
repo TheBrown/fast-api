1b9eb3a574ba184dcf95efc5c43ca22edd2880e9
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-23 21:59:22 +0700

    RedirectResponse

--- advanced/main.py
+++ advanced/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from fastapi.responses import UJSONResponse
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
-@app.get("/items/", response_class=UJSONResponse)
-async def read_items():
-    return [{"item_id": "Foo"}]
+@app.get("/typer")
+async def redirect_typer():
+    return RedirectResponse("https://typer.tiangolo.com")
+
+
+@app.get("/saleumsack", response_class=RedirectResponse)
+async def redirect_fastapi():
+    return "https://saleumsack.com"
