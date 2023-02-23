994e63f4553dfa50214220ee008e11da23c251ab
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-23 21:53:50 +0700

    PlainTextResponse

--- advanced/main.py
+++ advanced/main.py
@@ -1,19 +1,9 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 
-@app.get("/legacy/")
-def get_legacy_data():
-    data = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1> Look my! HTML!</h1>
-            <p>this is so nice though </p>
-        </body>
-    </html>
-    """
-    return Response(content=data, media_type="application/xml")
+@app.get("/", response_class=PlainTextResponse)
+async def main():
+    return "Hello World"
