c5a874988d8b39d625a5b1cb0b038a6d0de491d2
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-25 20:34:30 +0700

    Return a Response directly

--- advanced/main.py
+++ advanced/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-@app.get('/headers-and-object')
-def get_headers(response: Response):
-    response.headers["X-Cat-Dog"] = "alone in the world"
-    return {"message": "Hello World"}
+@app.get("/headers")
+def get_headers():
+    content = {"message": "Hello World"}
+    headers = {"X-Cat-Dog": "alone in the world", "Content-Language": "en-US"}
+    return JSONResponse(content=content, headers=headers)
