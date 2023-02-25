8637814e18a45f9514941ecbdc50c2aa8af4def4
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-25 19:47:45 +0700

    Additional Response with model

--- advanced/main.py
+++ advanced/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI, Response
-from fastapi.responses import ORJSONResponse
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
-app = FastAPI(default_response_class=ORJSONResponse)
+
+class Item(BaseModel):
+    id: str
+    value: str
 
 
-@app.get("/items")
-async def read_items():
-    return [{"item_id": "Foo"}]
+class Message(BaseModel):
+    message: str
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}", response_model=Item, responses={404: {"message": Message}})
+async def read_item(item_id: str):
+    if item_id == "foo":
+        return {"id": "foo", "value": "there goes my hero"}
+    return JSONResponse(status_code=404, content={"message": "Item not found"})
