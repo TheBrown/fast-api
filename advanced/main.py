da42c70c60b3161dbd817e2dbdcb76bcc52e3ac0
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-25 20:37:41 +0700

    Response - Change Status Code

--- advanced/main.py
+++ advanced/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
+tasks = {"foo": "Listen to the Bar Fighters"}
 
-@app.get("/headers")
-def get_headers():
-    content = {"message": "Hello World"}
-    headers = {"X-Cat-Dog": "alone in the world", "Content-Language": "en-US"}
-    return JSONResponse(content=content, headers=headers)
+
+@app.put("/get-or-create-task/{task_id}", status_code=200)
+def get_or_create_task(task_id: str, response: Response):
+    if task_id not in tasks:
+        tasks[task_id] = "This didn't exist before"
+        response.status_code = status.HTTP_201_CREATED
+    return tasks[task_id]
