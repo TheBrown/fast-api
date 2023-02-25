b45b2e69f3d9f3b6f78118d75b4820991c5219d3
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-02-25 20:25:14 +0700

    Return a Response directly

--- advanced/main.py
+++ advanced/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.post("/cookie-and-object")
 def create_cookie(response: Response):
+    content = {"message": "Come to the dark side, we have cookies"}
+    response = JSONResponse(content)
     response.set_cookie(key="fakesession", value="fake-cookie-session-value")
-    return {"message": "Come to the darke side, we have cookies"}
+    return response
+
