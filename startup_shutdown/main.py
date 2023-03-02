1544c1c706dffbbf78286c89df3f7f2acb9b729c
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-03-02 22:20:57 +0700

    Events: shutdown

--- startup_shutdown/main.py
+++ startup_shutdown/main.py
@@ -5,12 +5,12 @@ app = FastAPI()
 items = {}
 
 
-@app.on_event("startup")
-async def startup_event():
-    items["foo"] = {"name": "Fighters"}
-    items["bar"] = {"name": "Tenders"}
+@app.on_event("shutdown")
+async def shutdown_event():
+    with open("log.txt", mode="a") as log:
+        log.write("Application shutdown")
 
 
-@app.get("/items/{item_id}")
-async def read_items(item_id: str):
-    return items[item_id]
+@app.get("/items")
+async def read_items():
+    return [{"name": "Foo"}]
