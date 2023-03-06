6904d6b8c2fccd446a523477591bc1ee008d898b
Saleumsack KEOBOUALAY <saleumsack16@gmail.com>
2023-03-06 22:38:00 +0700

    Settings in another module

--- env/main.py
+++ env/main.py
@@ -1,3 +1,13 @@
-import os
-name = os.getenv("MY_NAME", "Bobby")
-print(f"Hello {name} from Python")
+from fastapi import FastAPI
+from .config import settings
+
+app = FastAPI()
+
+
+@app.get("/info")
+async def info():
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user
+    }
